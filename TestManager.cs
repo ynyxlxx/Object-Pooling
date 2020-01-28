10b5184afebf239de1331d6760c88d92ca8401de
ynyxlxx <lxx0209@outlook.com>
2020-01-28 16:47:37 +0800

    Add files via upload

---
+++ TestManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour {
+    public GameObject prefab;
+
+    void Start() {
+        PoolManager.instance.CreatePool(prefab, 30);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            PoolManager.instance.ReuseObject(prefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
---
+++ TestObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : PoolObject {
+
+    TrailRenderer trail;
+    float trailTime;
+
+    void Awake() {
+        trail = GetComponent<TrailRenderer>();
+        trailTime = trail.time;
+    }
+
+    void Update() {
+        transform.localScale += Vector3.one * Time.deltaTime * 3;
+        transform.Translate(Vector3.forward * Time.deltaTime * 25);
+    }
+
+    public override void OnObjectReuse() {
+        trail.time = -1;
+        Invoke("ResetTrail", .1f);
+        transform.localScale = Vector3.one;
+    }
+
+    void ResetTrail() {
+        trail.time = trailTime;
+    }
+}
